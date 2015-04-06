5d3c149bc8a37d6892ace87fd9abc17cd74ab3f1
Fredrik Carlsen <fredrik@carlsen.io>
2015-04-06 15:32:56 +0200

    Adds basic functionality and tests

--- docker/manager.py
+++ docker/manager.py
@@ -41,11 +41,16 @@ class Docker(object):
     def run(self, cmd, working_directory=""):
         if working_directory:
             cmd = "cd {0} && {1}".format(working_directory, cmd)
+        else:
+            cmd = "cd ~/ && {0}".format(cmd)
 
         return _execute('docker exec -i -t {0} bash -c "{1}"'.format(self.container_name, cmd))
 
     def read_file(self, path):
         return self.run("cat {0}".format(path)).out
+
+    def create_file(self, path, content):
+        return self.run("echo '{0}' >> {1}".format(content, path))
 
     def file_exist(self, path):
         return self.run("test -f {0}".format(path)).return_code == 0
@@ -53,28 +58,29 @@ class Docker(object):
     def directory_exist(self, path):
         return self.run("test -d {0}".format(path)).return_code == 0
 
-    def _list_all_files_and_directories(self, path):
-        result = []
-        for file_path in self.run("ls -m {0}".format(path)).out.split(", "):
-            result.append(file_path)
-        return result
-
     def list_files(self, path):
         result = []
 
         for file_path in self.run("ls -m {0}".format(path)).out.split(", "):
-            if self.file_exist(path + file_path):
+            full_path = os.path.join(path, file_path)
+            if self.file_exist(full_path) and not self.directory_exist(full_path):
                 result.append(file_path)
 
         return result
 
-    def list_directories(self, path):
+    def list_directories(self, path, include_trailing_slash=True):
         result = []
 
+        if path == "":
+            path = "~/"
+
         for file_path in self.run("cd {0} && ls -dm */".format(path)).out.split(", "):
-            result.append(file_path)
-            # if self.directory_exist(path+file_path):
-            #    result.append(file_path)
+            if self.directory_exist(os.path.join(path, file_path)):
+
+                if include_trailing_slash:
+                    result.append(file_path)
+                else:
+                    result.append(file_path[:-1])
 
         return result
 
--- docker/tests.py
+++ docker/tests.py
@@ -1,16 +1,48 @@
+import unittest
+from random import randint
 from manager import Docker
 
-with Docker() as docker:
 
-    docker.run("mkdir builds")
-    docker.run("touch builds/readme.txt")
-    docker.run("mkdir builds/3")
-    docker.run("mkdir builds/4")
-    docker.run("touch builds/3/README.txt")
-    docker.run("touch builds/3/test.txt")
-    docker.run("touch builds/4/test.txt")
+class DockerBasicInteraction(unittest.TestCase):
+    def test_create_files(self):
+        with Docker() as docker:
+            docker.run("touch file1")
+            docker.run("touch file2")
+            self.assertEqual(docker.list_files(""), ["file1", "file2"])
+
+    def test_create_directories(self):
+        with Docker() as docker:
+            docker.run("mkdir dir1")
+            docker.run("mkdir dir1/test")
+            docker.run("mkdir dir2")
+            docker.run("mkdir dir3")
+            self.assertEqual(docker.list_directories("", include_trailing_slash=False),
+                             ["dir1", "dir2", "dir3"])
+
+    def test_create_and_list_files_in_sub_directory(self):
+        with Docker() as docker:
+            docker.run("mkdir builds")
+            docker.run("touch builds/readme.txt")
+
+            self.assertEqual(docker.list_files("builds"), ["readme.txt"])
+
+    def test_create_file_with_content(self):
+        with Docker() as docker:
+            file_name = "readme.txt"
+            file_content = "this is a test file"
+
+            self.assertFalse(docker.file_exist(file_name))
+            docker.create_file(file_name, file_content)
+            self.assertTrue(docker.file_exist(file_name))
+
+    def test_read_file_with_content(self):
+        with Docker() as docker:
+            file_name = "readme.txt"
+            file_content = "this is a test file {0}".format(randint(5000, 5500))
+            docker.create_file(file_name, file_content)
+
+            self.assertEqual(docker.read_file(file_name), file_content)
 
 
-    print(docker._list_all_files_and_directories("/builds"))
-    print(docker.list_directories("/builds"))
-    #print(docker.list_files("/builds"))
+if __name__ == '__main__':
+    unittest.main()
